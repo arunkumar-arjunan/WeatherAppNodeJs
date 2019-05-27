64f7bb9feb6900e20f5d74364f698bb22c9c69ad
aruntracer <aruntracer@gmail.com>
2019-05-27 13:41:07 +0530

    Serve stylesheets statically with nginx.

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit', (event) => {
     locationMsg.textContent = 'Loading...';
     forecastMsg.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    fetch('/weather?address=' + search.value).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 locationMsg.textContent = data.error;
